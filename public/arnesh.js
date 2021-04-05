4df2f96d3a26f65c5c661236b7a35206631e9333
William Ray <wray@umass.edu>
2021-04-05 17:07:34 -0400

    Arnesh Javascript

---
+++ public/arnesh.js
@@ -0,0 +1,12 @@
+function visitConv(){
+    window.location='https://thawing-journey-93625.herokuapp.com/william';
+}
+function visitFAQ(){
+    window.location='https://thawing-journey-93625.herokuapp.com/jared';
+}
+function visitProfile(){
+    window.location='https://thawing-journey-93625.herokuapp.com/omri';
+}
+function visitLogIn(){
+    window.location='https://thawing-journey-93625.herokuapp.com/mike';
+}
